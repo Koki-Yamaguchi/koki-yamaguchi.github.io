d323328e6bdebc49a7f48a4cc42623e861a8ecf2
Koki Yamaguchi <yk49at21@gmail.com>
2018-03-23 16:41:56 +0900

    changed how event works

--- generator.py
+++ generator.py
@@ -33,7 +33,7 @@ def form():
     <form id="userid">
         <div class="ui action input">
             <input autocomplete="off" type="text" name="user" value="" placeholder="Search user ID" />
-            <button class="ui icon button" type="button" id="btn">
+            <button class="ui icon button" type="submit" id="btn">
                 <i class="search icon"></i>
             </button>
         </div>
